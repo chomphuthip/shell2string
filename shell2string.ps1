83d8f1eb37db75e0e4375a27cdb9a227af6c67d7
Gibson Chomphuthip <122696634+chomphuthip@users.noreply.github.com>
2023-01-15 15:09:12 -0600

    Added script

---
+++ shell2string.ps1
@@ -0,0 +1,4 @@
+﻿$RawBytes = [System.IO.File]::ReadAllBytes((Resolve-Path $args[0]).ToString())
+$MACStyle = [BitConverter]::ToString($RawBytes)
+$Cstyle = '\x' + $MACStyle.replace('-', '\x')
+Write-Host $Cstyle
